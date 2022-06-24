563bbfa3c73e312ca8aded62a49296c83bc67dfa
mehedi hasan <mehedi2592000@gmail.com>
2022-06-24 20:01:22 +0600

    start project

---
+++ Aiub_Hand_To_Hand_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aiub_Hand_To_Hand_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
